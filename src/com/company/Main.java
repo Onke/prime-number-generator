81ee67c30c97b798ebac3159d081597d6dfc4641
vince <sipukaom@gmail.com>
2021-03-28 23:13:30 +0200

    Brute force algorithm

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> primes = primeNumbersBruteForce(10000);
+        for(int i : primes)
+            System.out.print(i + " , ");
+    }
+
+    public static List<Integer> primeNumbersBruteForce(int n) {
+        List<Integer> primeNumbers = new LinkedList<>();
+        if (n >= 2) {
+            primeNumbers.add(2);
+        }
+        for (int i = 3; i <= n; i += 2) {
+            if (isPrimeBruteForce(i)) {
+                primeNumbers.add(i);
+            }
+        }
+        return primeNumbers;
+    }
+    private static boolean isPrimeBruteForce(int number) {
+        for (int i = 2; i*i <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
